6b8b7fa191f47dbbfa77d6ef575ba685bdba1e14
jisshub <jissmon476.com>
2022-03-17 13:33:17 +0530

    useCallback hook for functional dependancies

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useCallback } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import './TripList.css';
@@ -6,29 +7,36 @@ import './TripList.css';
 export default function TripList() {
 
     const [trips, setTrips] = useState([]);
+    const [url, setUrl] = useState('http://localhost:3000/trips');
+
+    const fetchTrips = useCallback(async() => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setTrips(data);
+    }, [url])
 
     useEffect(() => {
-        fetch('http://localhost:3000/trips')
-        .then(response => response.json())
-        .then(trips => {
-            setTrips(trips);
-        });
-    }, []);
+        fetchTrips();
+    }, [fetchTrips]);
     console.log(trips); 
-
+    
   return (
-    <div>
+    <div className='trip-list'>
       <h1>Trip List</h1>
       {
-        trips.map(trip => (
-            <ul className='trip-list'>
+        trips.map(trip =>  (
+            <ul>
                 <li key={trip.id}>
                     <h3>{trip.title}</h3>
-                    <p>{trip.price}</p>
+                    <p>{trip.price}</p>  
                 </li>
             </ul>
-        ))
-      }
+                ))
+            }
+            <div className='filter'>
+              <button onClick={() => setUrl('http://localhost:3000/trips?loc=india')}>Indian Trips</button>
+              <button onClick={() => setUrl('http://localhost:3000/trips')}>All Trips</button>
+            </div>
     </div>
   )
 }
